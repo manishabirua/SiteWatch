8f1e2ed3471ef86b8801a718a3370dd26b8a4342
manishabirua <manishabirua04@gmail.com>
2026-05-30 23:35:21 +0530

    Day 3: connected checker.py to Redis

--- checker/checker.py
+++ checker/checker.py
@@ -1,10 +1,13 @@
 import requests
 import time
 from datetime import datetime
+import redis
 
 URLS=["https://www.google.com", "https://www.youtube.com", "https://httpbin.org", "https://httpstat.us/503"]
+r = redis.Redis (host='localhost', port=6379, decode_responses=True)
 
 def check_url(url):
+ headers = {"User-Agents": "Mozilla/5.0"}
  try:
    response = requests.get(url, timeout=5)
    if response.status_code<500:
@@ -18,6 +21,7 @@ while True:
  for url in URLS:
    status = check_url(url)
    timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+   r.hset ("sitewatch", url, f"{status} | {timestamp}")
    print (f"[{timestamp}]  {url} -> {status}")
  print ("         (Waiting for 5 minutes)")
  time.sleep(300)
